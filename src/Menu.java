40a5f881192ae8748f79ebdb214d2be1974b2eb4
Richa Gupta <gptaricha@gmail.com>
2020-10-09 18:25:57 +0200

    Included documentation

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,10 @@
+/**
+ *  This class is to run the ToDoLy application
+ *  It contains only main method to be able to start the application
+ *
+ * @author Richa Gupta
+ * @version 2020.10.09
+ */
 public class Menu {
     public static void main (String [] args) {
         new ToDoLy().startToDoLy();
