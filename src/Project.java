9f473b30238f9ad8938090c5cde38e587abb6421
Richa Gupta <gptaricha@gmail.com>
2020-10-09 18:23:32 +0200

    added return projectId method

--- src/Project.java
+++ src/Project.java
@@ -1,3 +1,14 @@
+/**
+ *  This class is the main class of the "ToDoList" application.
+ *  "ToDoLy" allows the user to create new tasks, assign them a title and due date, and choose a project for that
+ *  task to belong to.
+ *
+ *  To be implemented
+ *
+ * @author Richa Gupta
+ * @version 2020.10.09
+ */
 public class Project {
-    protected int projectId;
+    protected String projectId;
+
 }
