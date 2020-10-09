626d8b779b33944a21cc0488e632aca081bedd33
Richa Gupta <gptaricha@gmail.com>
2020-10-09 18:08:08 +0200

    removed unwanted methods

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,54 +1,39 @@
 import java.util.Scanner;
 
+/**
+ * This class is part of the "To Do List" application.
+ * "To Do List" is a text based application to create new tasks, assign them a title  due date and group it under a project.
+ * <p>
+ * This InputReader class reads user input and tries to interpret it as an "Activity"
+ * that user wish to perform. Every time it is called it reads a line from the terminal and
+ * tries to interpret the line. It returns the command
+ * as String object
+ * <p>
+ * The parser has a set of known command words. It checks user input against
+ * the known commands, and if the input is not one of the known commands, it
+ * returns a command object that is marked as an unknown command.
+ *
+ * @author Richa Gupta
+ * @version 2020.10.09
+ */
 public class InputReader {
-    private static final String[] validUserInput = {"1","2","3","4"};
-    private Scanner reader;
-    public InputReader(){
+    private final Scanner reader;
+
+    /**
+     * Create a parser to read from the terminal window.
+     */
+    public InputReader() {
         reader = new Scanner(System.in);
+
     }
+
+    /**
+     * Returns commands from user
+     *
+     * @return
+     */
     public String getCommand() {
-        System.out.print("> ");     // print prompt
-        String inputLine = reader.nextLine();
-        return inputLine;
+        System.out.print(">> ");     // print prompt
+        return reader.nextLine().trim().toLowerCase();
     }
-
-    public boolean isValidCommand(String userInput) {
-        for(String input:validUserInput)
-        {
-            if(input.equalsIgnoreCase(userInput)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-  /*  public String getCommand() {
-        String inputLine;   // will hold the full input line
-        String word1 = null;
-        String word2 = null;
-
-        System.out.print("> ");     // print prompt
-
-        inputLine = reader.nextLine();
-
-        // Find up to two words on the line.
-        Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
-            word1 = tokenizer.next();      // get first word
-            if(tokenizer.hasNext()) {
-                word2 = tokenizer.next();      // get second word
-                // note: we just ignore the rest of the input line.
-            }
-        }
-
-        // Now check whether this word is known. If so, create a command
-        // with it. If not, create a "null" command (for unknown command).
-        if(commands.isCommand(word1)) {
-            return new Command(word1, word2);
-        }
-        else {
-            return new Command(null, word2);
-        }
-    }*/
 }
